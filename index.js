b543bd97d63ed79d13c18d77611aac8d99e9fda5
Angela Pace <angpace13@gmail.com>
2022-09-06 10:43:35 -0400

    commit tree df0bef89bde1cc773976e123493eb64669cd30d7

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// let names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(name, holiday){
+  let cart = []
+for (let i = 0; i < name.length; i++) {
+ cart.push(`Thank you, ${name[i]}, for the wonderful ${holiday} gift!`)
+
+}
+return cart
+
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+function countDown(){
+  let countDown = 10
+  while ( countDown > -1 ) { console.log(countDown) 
+    countDown--
+  }
+
+}
